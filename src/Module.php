19c832356b1260d9206870e841cb3b0ca273f7fb
JimmDiGrizli <getmansk-y@yandex.ru>
2014-05-15 23:02:35 +0400

    Added Module.php

---
+++ src/Module.php
@@ -0,0 +1,10 @@
+<?php
+namespace GetSky\DemoModule;
+
+use GetSky\Phalcon\Bootstrap\Module as ModuleBootstrap;
+
+class Module extends ModuleBootstrap
+{
+    const DIR = __DIR__;
+    const NAME = 'DemoModule';
+}
