e62b58acf65068efe81cf3491bef5451ae35524a
JimmDiGrizli <getmansk-y@yandex.ru>
2014-05-15 20:31:00 +0400

    Created demo module

---
+++ src/Controllers/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+namespace GetSky\DemoModule\Controllers;
+
+use \Phalcon\Mvc\Controller;
+
+class IndexController extends Controller
+{
+
+    public function indexAction()
+    {
+    }
+
+    public function aboutAction()
+    {
+    }
+
+    public function error404Action()
+    {
+    }
+
+}
